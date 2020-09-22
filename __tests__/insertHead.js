2afdc4dd57b9d03464ac341170ba448fd526101e
Matt-GitHub <matthewjamesbedard@outlook.com>
2020-09-22 08:25:27 -0400

    updated test to not rely on mutable object, created new linked list per test making it more robust and not dependent on other tests

--- __tests__/insertHead.js
+++ __tests__/insertHead.js
@@ -1,34 +1,35 @@
-import LinkedList from './index';
+import LinkedList from '../index';
 
 describe('testing insert head functionality of linked list', () => {
-  // initalize new linked list (empty)
-  let LL = new LinkedList();
   test('initalization of linked list should result in head and tail being null', () => {
+    let LL = new LinkedList();
     expect(LL.head && LL.tail).toBe(null);
   });
-  // insert value into empty linked list
   test('insertHead of empty linked list', () => {
+    let LL = new LinkedList();
     LL.insertHead('firstValue');
     expect(LL.head.value).toBe('firstValue');
     expect(LL.tail.value).toBe('firstValue');
     expect(LL.head.next && LL.tail.next).toBe(null);
     expect(LL.head.value || LL.tail.value).not.toBe(null);
   });
-
   test('insertHead of linked list with 1 node in list', () => {
+    let LL = new LinkedList();
+    LL.insertHead('firstValue');
     LL.insertHead('secondValue');
     expect(LL.head.value).toBe('secondValue');
     expect(LL.tail.value).toBe('firstValue');
     expect(LL.head.next.value).toBe('firstValue');
     expect(LL.tail.next).toBe(null);
   });
-
   test('insertHead of linked list with multiple nodes in list', () => {
+    let LL = new LinkedList();
+    LL.insertHead('firstValue');
+    LL.insertHead('secondValue');
     LL.insertHead('thirdValue');
     expect(LL.head.value).toBe('thirdValue');
     expect(LL.head.next.value).toBe('secondValue');
     expect(LL.tail.value).toBe('firstValue');
     expect(LL.tail.next).toBe(null);
   });
-  //   console.log('test look', LL);
 });
